0c1b74cfe5757f0eb4a72f93a85d48bd3d926fb4
unknown <jacobdeanr@gmail.com>
2024-05-15 16:44:08 -0500

    update detail page
    added more information on a given sky. This includes license, author, file size, and details (if applicable)

--- site/fetch_skyboxes.js
+++ site/fetch_skyboxes.js
@@ -21,11 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
     function displaySkyboxes(skyboxes, selectedTime, selectedWeather) {
         container.innerHTML = '';
         skyboxes.forEach(item => {
-            const itemCategories = categories[item.name];
-            if (!itemCategories) return;
+            const itemData = categories[item.name];
+            if (!itemData) return;
 
-            const hasSelectedTime = selectedTime ? itemCategories.includes(selectedTime) : true;
-            const hasSelectedWeather = selectedWeather ? itemCategories.includes(selectedWeather) : true;
+            const hasSelectedTime = selectedTime ? itemData.categories.includes(selectedTime) : true;
+            const hasSelectedWeather = selectedWeather ? itemData.categories.includes(selectedWeather) : true;
 
             if (hasSelectedTime && hasSelectedWeather) {
                 const itemElement = document.createElement('div');
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-
+    /*
 	function create3DPreview(basePath) {
 		const container = document.getElementById('preview-container');
 		container.innerHTML = '';
@@ -149,13 +149,112 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             })
             .catch(error => console.error('Error fetching the repository contents:', error));
+    }*/
+
+    function fetchSkyboxDetails(name) {
+        fetch('https://api.github.com/repos/Jacobdeanr/Source_Skyboxes/contents')
+            .then(response => response.json())
+            .then(data => {
+                const item = data.find(item => item.name === `${name}.7z`);
+                const itemData = categories[`${name}.7z`];
+                if (item && itemData) {
+                    const detailsContainer = document.getElementById('details-container');
+                    detailsContainer.innerHTML = '';
+    
+                    const title = document.createElement('h2');
+                    title.textContent = item.name.replace('.7z', '');
+    
+                    const img = document.createElement('img');
+                    img.src = `preview/${item.name.replace('.7z', '.jpg')}`;
+                    img.alt = item.name;
+    
+                    const link = document.createElement('a');
+                    link.href = item.download_url;
+                    link.textContent = 'Download';
+    
+                    const fileSize = document.createElement('p');
+                    fileSize.textContent = `File size: ${formatFileSize(item.size)}`;
+    
+                    const author = document.createElement('p');
+                    author.textContent = `Author: ${itemData.author}`;
+    
+                    const description = document.createElement('p');
+                    description.textContent = `Description: ${itemData.description}`;
+                    
+                    //Shenanigans to get the text color to appear right.
+                    const brightnessColor = `rgb(${itemData.sunParameters.brightness.slice(0, 3).join(', ')})`;
+                    const ambienceColor = `rgb(${itemData.sunParameters.ambience.slice(0, 3).join(', ')})`;
+                    const fogPrimaryColor = `rgb(${itemData.fogParameters.primaryFogColor.join(', ')})`;
+                    const fogSecondaryColor = `rgb(${itemData.fogParameters.secondaryFogColor.join(', ')})`;
+    
+                    const brightnessTextColor = getTextColor(itemData.sunParameters.brightness.slice(0, 3));
+                    const ambienceTextColor = getTextColor(itemData.sunParameters.ambience.slice(0, 3));
+                    const primaryFogTextColor = getTextColor(itemData.fogParameters.primaryFogColor);
+                    const secondaryFogTextColor = getTextColor(itemData.fogParameters.secondaryFogColor);
+                    
+                    //License
+                    const license = document.createElement('p');
+                    license.id = "license-info"
+                    license.innerHTML = `License: <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">
+                    Creative Commons Attribution 4.0 International (CC BY 4.0)</a>`;
+    
+                    console.log(brightnessColor);
+                    console.log(primaryFogTextColor);
+    
+                    const envParameters = `
+                        <tr>
+                            <td>${itemData.sunParameters.sunAngle}</td>
+                            <td>${itemData.sunParameters.pitch}</td>
+                            <td style="background-color: ${brightnessColor}; color: ${brightnessTextColor};">${itemData.sunParameters.brightness.join(' ')}</td>
+                            <td style="background-color: ${ambienceColor}; color: ${ambienceTextColor};">${itemData.sunParameters.ambience.join(' ')}</td>
+                            <td style="background-color: ${fogPrimaryColor}; color: ${primaryFogTextColor};">${itemData.fogParameters.primaryFogColor.join(' ')}</td>
+                            </tr>`;
+                            
+                            //<td style="background-color: ${fogSecondaryColor}; color: ${secondaryFogTextColor};">${itemData.fogParameters.secondaryFogColor.join(' ')}</td>
+                    const detailsTable = `
+                        <table>
+                            <tr>
+                                <th>Sun Angle</th>
+                                <th>Pitch</th>
+                                <th>Brightness</th>
+                                <th>Ambient</th>
+                                <th>Primary Fog Color</th>
+                                
+                            </tr>
+                            ${envParameters}
+                        </table>`;
+                            //<th>Secondary Fog Color</th>
+    
+                    detailsContainer.appendChild(title);
+                    detailsContainer.appendChild(img);
+                    detailsContainer.innerHTML += detailsTable; // Adding table as HTML string
+                    detailsContainer.appendChild(author);if (itemData.description.trim() !== "") {
+                        detailsContainer.appendChild(description);
+                    }
+                    detailsContainer.appendChild(fileSize);
+                    detailsContainer.appendChild(license);
+                    detailsContainer.appendChild(link);
+    
+                    document.getElementById('skybox-container').style.display = 'none';
+                    detailsContainer.style.display = 'block';
+                }
+            })
+            .catch(error => console.error('Error fetching the repository contents:', error));
     }
+    
 
     function formatFileSize(bytes) {
         const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
         if (bytes === 0) return '0 Byte';
         const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
         return `${(bytes / Math.pow(1024, i)).toFixed(2)} ${sizes[i]}`;
+    }
+
+    function getTextColor(rgbArray) {
+        const [r, g, b] = rgbArray;
+        const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+        console.log(luminance)
+        return luminance > 0.5 ? '#000000' : '#FFFFFF';
     }
 
     function router() {
