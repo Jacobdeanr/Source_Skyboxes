749762c53b57f1a1c51e8e8fb9bb97ca43f28b3c
Jacob Robbins <Jacobdeanr@gmail.com>
2025-04-02 17:03:02 -0500

    Automatic processing of thumbnails
    New skybox sky_sunset010_hdr

---
+++ _skyboxes/sky_sunset010_hdr.md
@@ -0,0 +1,36 @@
+---
+layout: skybox
+title: sky_sunset010_hdr - Skybox Texture
+skybox_name: sky_sunset010_hdr
+description: 
+author: Jacob Robbins
+publishDate: 2025-04-02
+license: CC BY 4.0
+sunParameters:
+  sunAngle: "0 159 0"
+  pitch: "-1"
+  brightness:
+    - 250
+    - 106
+    - 86
+    - 5
+  brightnessTextColor: "#000000"
+  ambience:
+    - 53
+    - 58
+    - 74
+    - 250
+  ambienceTextColor: "#FFFFFF"
+fogParameters:
+  primaryFogColor:
+    - 58
+    - 64
+    - 85
+  primaryFogColorTextColor: "#FFFFFF"
+  secondaryFogColor:
+    - 0
+    - 0
+    - 0
+  secondaryFogColorTextColor: "#FFFFFF"
+download_link: https://raw.githubusercontent.com/Jacobdeanr/Source_Skyboxes/master/sky_sunset010_hdr.7z
+---
--- build_and_run_site.bat
+++ build_and_run_site.bat
@@ -1,5 +1,25 @@
 @echo off
+REM Define directories relative to the current folder
+set "highResFolder=assets\images\high-res"
+set "thumbFolder=assets\images\thumbnails"
 
+REM Create thumbnails folder if it doesn't exist
+if not exist "%thumbFolder%" mkdir "%thumbFolder%"
+
+REM Process each JPEG file in the high-res folder
+for %%F in ("%highResFolder%\*.jpg") do (
+    echo Processing %%~nxF...
+    REM Resize and output as JPEG
+    magick "%%F" -resize 400x225 "%thumbFolder%\%%~nF.jpg"
+    REM Resize and output as WebP
+    magick "%%F" -resize 400x225 "%thumbFolder%\%%~nF.webp"
+)
+
+REM Mogrify both JPEG and WebP files in the thumbnail folder
+for %%e in (jpg webp) do (
+    magick mogrify -quality 85 "%thumbFolder%\*.%%e"
+)
+
+REM Build Site
 ruby generate_skybox_pages.rb
-
 bundle exec jekyll serve --force_polling
