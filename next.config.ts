af39b01d03b78ffb0316618908211c855ef2a94a
Jacob Robbins <Jacobdeanr@gmail.com>
2025-05-04 14:34:29 -0500

    Initial

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Source_Skyboxes_NextJS",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
