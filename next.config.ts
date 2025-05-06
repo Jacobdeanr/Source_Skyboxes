5533ef9f5fa4243034fc1975a3c6639a8a7b22a3
Jacob Robbins <Jacobdeanr@gmail.com>
2025-05-05 19:01:08 -0500

    Attempt to resolve asset configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Source_Skyboxes_NextJS",
+  assetPrefix: "/Source_Skyboxes_NextJS",
   images: {
     unoptimized: true,
   },
