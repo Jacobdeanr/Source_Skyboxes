f96a7b3af20ad94bfc8f6ae436c5f1cb30b1e5be
Jacob Robbins <Jacobdeanr@gmail.com>
2025-08-15 23:49:46 -0500

    Update not-found

--- app/not-found.js
+++ app/not-found.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 
 export default function NotFound() {
-  return <div>
+  return <div className="flex flex-col items-center justify-center h-screen">
       <h1>Not found - 404</h1>
-      <div>
-        <Link href="/">Go back to Home</Link>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <div className="mt-4">
+        <Link href="/">Click here to go back to the main page</Link>
       </div>
   </div>
 }
